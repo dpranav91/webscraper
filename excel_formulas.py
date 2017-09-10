10803ee87b69d3ef726cb7808b53d73dca66dc5d
pranav <d.pranav91@gmail.com>
2017-09-10 12:46:14 +0530

    Excel formulas updates

--- excel_formulas.py
+++ excel_formulas.py
@@ -3,14 +3,15 @@ from collections import OrderedDict as od
 formulas_dict = od([
     ('Spread', '=IF(AND(ISNUMBER(E{0}), ISNUMBER(U{0})),U{0}-E{0},"")',),
     ('Spread %', '=IF(AND(ISNUMBER(X{0}), ISNUMBER(U{0}), U{0}<>0), X{0}/U{0}, "")'),
-    ('Loan Amount', '=IF(AND(ISNUMBER(E2)),E2*0.75, "")'),
-    ('Mortage Princ+Int', '=IF(AND(ISNUMBER(Z2)),-PMT(4.25/1200,120, Z2), "")'),
-    ('Property Tax', '=IF(AND(ISNUMBER(U2)),U2*0.0125/12, "")'),
-    ('HOA+INS', '170+(400/12)'),
-    ('Final Mortgage', '=IF(AND(ISNUMBER(AA2), ISNUMBER(AB2),  ISNUMBER(AC2)), AA2+AB2+AC2, "")'),
+    ('Loan Amount', '=IF(AND(ISNUMBER(E{0})),E{0}*0.75, "")'),
+    ('Mortage Princ+Int', '=IF(AND(ISNUMBER(Z{0})),-PMT(4.25/1200,120, Z{0}), "")'),
+    ('Property Tax', '=IF(AND(ISNUMBER(U{0})),U{0}*0.0125/12, "")'),
+    ('HOA+INS', '=170+(400/12)'),
+    ('Final Mortgage',
+     '=IF(AND(ISNUMBER(AA{0}), ISNUMBER(AB{0}),  ISNUMBER(AC{0})), AA{0}+AB{0}+AC{0}, "")'),
 
     ('Rento Meter Avg', ''),
     ('Rent Best', ''),
     ('Rest Bottom', ''),
-    ('Break Even', '=IF(AND(ISNUMBER(AE2), ISNUMBER(AD2)), (AE2-AD2),"")'),
+    ('Break Even', '=IF(AND(ISNUMBER(AE{0}), ISNUMBER(AD{0})), (AE{0}-AD{0}),"")'),
 ])
